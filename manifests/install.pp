337ddbcb8ce5e8c15c375e12c6afa52e5ee4a98e
root <root@vivdash1.mylabserver.com>
2018-02-16 13:39:40 +0000

    updated clasess

---
+++ manifests/install.pp
@@ -0,0 +1,8 @@
+class nginx::install(
+  String $ensure = $::nginx::ensure,
+  String $package_name = $::nginx::package_name,){
+  package { 'install_nginx':
+    ensure => $ensure,
+    name   => $package_name,
+  }
+}
---
+++ manifests/service.pp
@@ -0,0 +1,18 @@
+class nginx::service(
+  String $service_name = $::nginx::service_name,
+  String $ensure = $::nginx::service_ensure,
+  String $pattern = $::nginx::service_pattern,
+  Boolean $enable = $::nginx::service_enable,
+  Boolean $hasstatus = $::nginx::service_hasstatus,
+  Boolean $hasrestart = $::nginx::service_hasrestart,
+  String $docroot = $::nginx::params::docroot,){
+  service { 'nginx_service':
+    ensure => $ensure,
+    name => $service_name,
+    enable => $enable,
+    hasstatus => $hasstatus,
+    hasrestart => $hasrestart,
+    pattern => $pattern,  
+  
+  }
+}
